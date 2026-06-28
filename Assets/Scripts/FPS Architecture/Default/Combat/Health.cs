22a8091ccb467fad880f32ea35b5156e69bd4862
23raven <23raven.games@gmail.com>
2026-06-28 20:10:45 +0500

    add Health.cs

---
+++ Assets/Scripts/FPS Architecture/Default/Combat/Health.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float maxHealth = 100f;
+
+    public float CurrentHealth { get; private set; }
+
+    private void Awake()
+    {
+        CurrentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= damage;
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        CurrentHealth += amount;
+        CurrentHealth = Mathf.Min(CurrentHealth, maxHealth);
+    }
+
+    private void Die()
+    {
+        Debug.Log($"{name} died.");
+    }
+}
