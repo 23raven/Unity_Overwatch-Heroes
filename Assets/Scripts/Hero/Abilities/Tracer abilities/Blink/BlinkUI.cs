460cda1f1315899cbec4e34cc63bdc48d1267d30
23raven <23raven.games@gmail.com>
2026-07-20 16:50:03 +0500

    add blink ui

---
+++ Assets/Scripts/Hero/Abilities/Tracer abilities/Blink/BlinkUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkUI : MonoBehaviour
+{
+    [System.Serializable]
+    private class ChargeUI
+    {
+        public Image Empty;
+        public Image Fill;
+    }
+
+    [SerializeField] private ChargeUI[] charges;
+
+    /// <summary>
+    /// availableCharges = сколько зарядов сейчас доступно.
+    /// rechargeProgress = прогресс восстановления следующего заряда (0-1).
+    /// </summary>
+    public void UpdateCharges(int availableCharges, float rechargeProgress)
+    {
+        for (int i = 0; i < charges.Length; i++)
+        {
+            ChargeUI charge = charges[i];
+
+            if (i < availableCharges)
+            {
+                charge.Fill.gameObject.SetActive(true);
+                charge.Fill.fillAmount = 1f;
+            }
+            else if (i == availableCharges)
+            {
+                charge.Fill.gameObject.SetActive(true);
+                charge.Fill.fillAmount = rechargeProgress;
+            }
+            else
+            {
+                charge.Fill.gameObject.SetActive(false);
+            }
+        }
+    }
+}
